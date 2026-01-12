ae3c58a805dfc892affd0dbd849b35fd3935ac2b
Tarsis Lima <br.tmvdl@gmail.com>
2026-01-12 17:18:12 -0300

    Update project files

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+const access = (_, res) => res
+  .setHeader('Access-Control-Allow-Origin', '*')
+  .setHeader('Access-Control-Allow-Headers', '*')
+  .setHeader('Access-Control-Allow-Methods', '*')
+  .setHeader('Access-Control-Allow-Credentials', true)
+
+app.head('/', access)
+
+app.options('/', access)
+
 app.post('/', (req, res) => {
   const token = process.env.TELEGRAM_TOKEN
   const chatId = process.env.TELEGRAM_CHAT_ID
