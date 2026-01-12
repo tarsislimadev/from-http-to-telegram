93528084dbb1d96d8d9f3ac34d4c6b76f9abbc55
Tarsis Lima <br.tmvdl@gmail.com>
2026-01-12 16:46:11 -0300

    Add project files

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.post('/', (req, res) => {
+  res.json({ status: 'ok' })
+})
+
+module.exports = app
