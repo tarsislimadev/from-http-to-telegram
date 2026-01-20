8e207362874c2f79dd999b509653b29a911995ea
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-01-20 21:03:33 +0000

    Add Translate library files

---
+++ libs/translate/index.js
@@ -0,0 +1,9 @@
+const trans = {
+  "en-us": require("./enus.js"),
+  "es-es": require("./eses.js"),
+  "pt-br": require("./ptbr.js"),
+};
+
+module.exports.trans = (sentence, context) => {
+  // FIXME
+}
